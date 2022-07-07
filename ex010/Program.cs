daef590838cf09553df66a667108864560466d71
Natalya <h.h.smolnikova@mail.ru>
2022-07-07 21:38:21 +0500

    задача 10

---
+++ ex010/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход трехзначное число и
+        // на выходе показывает вторую цифру этого числа
+        Console.WriteLine("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string str = number.ToString();
+        char ch = str[1];
+        Console.WriteLine(ch);
+    }
+}
+}
+
+    
+    
+        
