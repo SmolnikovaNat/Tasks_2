b8c3597eddd0a9fcd85f8994819c0a212b37008a
Natalya <h.h.smolnikova@mail.ru>
2022-07-08 21:57:46 +0500

    задача 11

---
+++ ex011/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+        //что третьей цифры нет
+        
+        Console.WriteLine("Введите трехзначное число: ");
+        int anyNumber = Convert.ToInt32(Console.ReadLine());
+        string anyNumberText = Convert.ToString(anyNumber);
+        if (anyNumberText.Length > 2)
+            
+        {
+            Console.WriteLine(" -> " + anyNumberText[2]);
+        }
+        else
+        {
+            Console.WriteLine("-> третьей цифры нет");
+        }
+    }
+}
+}
