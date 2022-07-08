b59f320392aa6024f767258f40f18045b05bb97e
Natalya <h.h.smolnikova@mail.ru>
2022-07-08 20:36:07 +0500

    задача 15

---
+++ ex15/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход цифру, обозначающую день недели
+        //и проверяет является ли этот день выходным
+
+             
+        Console.WriteLine("Введите число недели: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        if (x==1)
+        {
+            Console.WriteLine("нет");
+        }
+        if (x==2)
+        {
+            Console.WriteLine("нет");
+        }
+        if (x==3)
+        {
+            Console.WriteLine("нет");
+        }
+        if (x==4)
+        {
+            Console.WriteLine("нет");
+        }
+        if (x==5)
+        {
+            Console.WriteLine("нет");
+        }if (x==6)
+        {
+            Console.WriteLine("да");            
+        }
+        if (x==7)
+        {
+            Console.WriteLine("да");
+        }
+       
+    }
+}
+}
